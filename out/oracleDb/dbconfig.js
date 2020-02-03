cb64e8ed09112dfb5af61ebca51d3e8769428aa4
recokioshi <recokioshi@gmail.com>
2020-02-03 11:51:03 +0100

    removes build path

--- out/oracleDb/dbconfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-require('dotenv').config();
-exports["default"] = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    connectString: "(DESCRIPTION =\n    (ADDRESS_LIST =\n      (ADDRESS = (PROTOCOL = TCP)(HOST = sv-ora01-psdev1.cid31tg8iien.eu-central-1.rds.amazonaws.com)(PORT = 1521))\n    )\n    (CONNECT_DATA =\n      (SID = axfour)\n    )\n  )"
-};
-//# sourceMappingURL=dbconfig.js.map
